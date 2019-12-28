b6c519b0693252770cf7f26ebabc1575dae50cf3
Alan G. Garcia Rojas <aggarcia@grupomavi.com>
2019-12-27 18:55:34 -0600

    first commit

---
+++ src/main/kotlin/NullableTypes.kt
@@ -0,0 +1,16 @@
+fun main(){
+}
+fun sendMessageToClient(
+    client: Client?, message: String?, mailer: Mailer
+){
+    if(client != null && message != null){
+        client.personalInfo?.email?.let{
+            mailer.sendMessage(it , message)
+        }
+    }
+}
+class Client (val personalInfo: PersonalInfo?)
+class PersonalInfo (val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
